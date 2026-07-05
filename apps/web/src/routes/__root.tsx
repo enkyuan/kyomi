d57e4d63f3d23ae0f3addbbeaf2e13cb0808333b
enkyuan <yuan_enkang@outlook.com>
2026-07-04 19:29:30 -0500

    chore: updated favicons in __root.tsx

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -77,11 +77,18 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       {
         rel: "icon",
         type: "image/png",
-        sizes: "96x96",
-        href: "/favicon/favicon-96x96.png",
+        sizes: "32x32",
+        href: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon/favicon-16x16.png",
       },
       {
         rel: "apple-touch-icon",
+        sizes: "180x180",
         href: "/favicon/apple-touch-icon.png",
       },
       {
